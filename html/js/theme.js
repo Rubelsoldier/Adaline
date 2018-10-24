ad179f7e60a5e08bef8752bfc12b30acd3c5ede7
Rubel Rana <rubeltoahmed@gmail.com>
2018-10-24 19:49:26 +0600

    transitioned slide added, tough not working properly

--- html/js/theme.js
+++ html/js/theme.js
@@ -69,20 +69,67 @@ $('.navbar-collapse ul li a').click(function() {
 
 // >>>>>>>>>>>>>>>>>>>>>> Owl carousel <<<<<<<<<<<<<<<<<<<<< 
     
-    $("#owl-demo").owlCarousel({
- 
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
- 
-  });
+   /*JS way for setting height: 100vh to slides' height*/
+/*const $slides = $(".owl-carousel .owl-slide");
+                                                                     $slides.css("height", $(window).height());
+                                                                     $(window).resize(() => {
+                                                                       $slides.css("height", $(window).height());
+                                                                     });*/
+
+$(".owl-carousel").on("initialized.owl.carousel", function () {
+  setTimeout(function () {
+    $(".owl-item.active .owl-slide-animated").addClass("is-transitioned");
+    $("section").show();
+  }, 200);
+});
+
+var $owlCarousel = $(".owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  nav: true,
+  navText: [
+  '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
+  '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>' /* icons from https://iconmonstr.com */] });
+
+
+
+$owlCarousel.on("changed.owl.carousel", function (e) {
+  $(".owl-slide-animated").removeClass("is-transitioned");
+
+  var $currentOwlItem = $(".owl-item").eq(e.item.index);
+  $currentOwlItem.find(".owl-slide-animated").addClass("is-transitioned");
+
+  var $target = $currentOwlItem.find(".owl-slide-text");
+  doDotsCalculations($target);
+});
+
+$owlCarousel.on("resize.owl.carousel", function () {
+  setTimeout(function () {
+    setOwlDotsPosition();
+  }, 50);
+});
+
+/*if there isn't content underneath the carousel*/
+//$owlCarousel.trigger("refresh.owl.carousel");
+
+setOwlDotsPosition();
+
+function setOwlDotsPosition() {
+  var $target = $(".owl-item.active .owl-slide-text");
+  doDotsCalculations($target);
+}
+
+function doDotsCalculations(el) {
+  var height = el.height();var _el$position =
+  el.position(),top = _el$position.top,left = _el$position.left;
+  var res = height + top + 20;
+
+  $(".owl-carousel .owl-dots").css({
+    top: res + "px",
+    left: left + "px" });
+
+}
+
+
 
   });
